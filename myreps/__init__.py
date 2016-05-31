1926f981edeeb36d6bfa32e0708dc9839b0af885
Rafael Sandroni <rafaelsandronidias@gmail.com>
2016-05-30 21:33:43 -0300

    update, run without neo4j

--- myreps/__init__.py
+++ myreps/__init__.py
@@ -1,10 +1,10 @@
 from .views import app
-from .models import graph
+#from .models import graph
 
 def create_uniqueness_constraint(label, property):
     query = "CREATE CONSTRAINT ON (n:{label}) ASSERT n.{property} IS UNIQUE"
     query = query.format(label=label, property=property)
-    graph.cypher.execute(query)
+    #graph.cypher.execute(query)
 
 create_uniqueness_constraint("User", "username")
 create_uniqueness_constraint("Tag", "name")
