5b68e3d834620cdf16612abd3e0079a33a93594e
Rafael Sandroni <rafaelfsandroni@gmail.com>
2016-06-06 18:43:18 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,7 @@ import os
 import sys
 import site
 
-# Add the site-packages of the chosen virtualenv to work with
-#site.addsitedir('/home/rafael/Python/MyReputation/myprojectenv/local/lib/python2.7/site-packages')
-
-# Add the app's directory to the PYTHONPATH
-#sys.path.append('/home/rafael/Python/MyReputation')
-
-# Activate your virtual env
+# virtual env runing on Apache
 activate_this="/home/rafael/Python/MyReputation/myprojectenv/bin/activate_this.py"
 execfile(activate_this, dict(__file__=activate_this))
 
